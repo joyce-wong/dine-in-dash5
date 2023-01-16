df372b97bfd065919e31c5bcd5ac384768eeb300
Joyce-Wong <joycekwo12@gmail.com>
2023-01-16 17:14:29 -0500

    logo added to navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -5,14 +5,15 @@ import {logout} from '../store'
 import { Button } from '@material-ui/core'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div className="logo-container">
+  <div>
     {/* <img className="logo" src="/assets/images/logo.png"/> */}
     <nav>
       {isLoggedIn ? (
         <div className="nav-container">
           {/* The navbar will show these links after you log in */}
-      <div className="logo">
-        <h1>My Journal</h1>
+      <div className="logo-container">
+        <h1 id="title" className="logo-items">My Journal</h1>
+      <img id="logo-image" className="logo-items" style={{display: 'block', marginRight: 'auto', marginLeft: 'auto', height: '10vh',}} src="assets/images/logo.png" alt="logo" w="10%"/>
       </div>
       <ul className="nav-links">
           <li className="nav-item"><Button href="/home" color="primary" variant="contained">
@@ -26,8 +27,9 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div className='nav-container'>
           {/* The navbar will show these links before you log in */}
-        <div className="logo">
-          <h1>My Journal</h1>
+        <div className="logo-container">
+          <h1 id="title" className="logo-items">My Journal</h1>
+          <img id="logo-image" className="logo-items" style={{display: 'block', height: '10vh'}} src="assets/images/logo.png" alt="logo" w="10%"/>
         </div>
         <ul className="nav-links">
           <li className="nav-item"><Link to="/login">Login</Link></li>
