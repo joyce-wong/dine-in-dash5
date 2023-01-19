05fd841772a245104f9cf663cb1c95c584e7a17a
Joyce-Wong <joycekwo12@gmail.com>
2023-01-19 17:27:05 -0500

    added welcome image to assets

--- client/components/WelcomePage.js
+++ client/components/WelcomePage.js
@@ -6,7 +6,7 @@ class WelcomePage extends Component{
             <main>
                 <div id="welcome">      
                     <h2>Welcome to My Journal.</h2>
-                    <img style={{display: 'block', marginRight: 'auto', marginLeft: 'auto', height: '100vh',}} src="https://ww2.kqed.org/app/uploads/sites/23/2018/01/iStock-911463836-1920x1080.jpg" />    
+                    <img style={{display: 'block', marginRight: 'auto', marginLeft: 'auto', height: '100vh',}} src="assets/images/welcome.jpg" />    
                 </div>
             </main>
         )
